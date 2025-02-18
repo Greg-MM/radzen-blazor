9e1a34fb5c54469fc7f304a834faa8da1d51062a
TSE <Administrator@TranExec.com>
2025-02-18 11:12:03 -0500

    Transfer / Before .NET 9

---
+++ Radzen.Blazor/DropDownAutoCompleteItemRenderEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radzen.Blazor
+{
+	/// <summary>
+	/// Supplies information about RadzenDropDownAutoComplete ItemRender event.
+	/// </summary>
+	public class DropDownAutoCompleteItemRenderEventArgs<TValue> : DropDownBaseItemRenderEventArgs<TValue>
+	{
+		/// <summary>
+		/// Gets the DropDown.
+		/// </summary>
+		public RadzenDropDownAutoComplete<TValue> DropDown { get; internal set; }
+	}
+}
